97d2f1201c6d0975dc0c89ee89a117b4a80588cb
yangfuzhang <yangfuzhang0720@126.com>
2017-10-23 10:47:13 +0800

    add selectionSort.js

---
+++ selectionSort.js
@@ -0,0 +1,36 @@
+//javascript选择排序
+
+
+/*
+**@param array
+*/
+function SelectionSort(array) {
+    var length = array.length,
+        indexMin;
+    
+    for(var i = 0; i < length - 1; i++) {
+		indexMin = i;
+		for(var j = i+1; j < length;j++) {
+			if(array[j] < array[indexMin]) {
+				indexMin = j;
+			}
+		}
+		
+		if(indexMin != i) {
+			swap(array, i, indexMin);
+		}
+	}	
+	
+	console.log(array)
+}
+
+/*辅助函数，交换数组元素
+**@param array
+**@param a, b (数组索引)
+*/
+function swap(array, a, b) {
+	var temp = array[a];
+	array[a] = array[b];
+	array[b] = temp;
+}
+
