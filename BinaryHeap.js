699c4d2b75faef05196d523022ad2a7c2f550e72
yangfuzhang <yangfuzhang0720@126.com>
2020-12-30 18:26:00 +0800

    add: new item

--- BinaryHeap.js
+++ BinaryHeap.js
@@ -4,22 +4,34 @@ class MaxHeap {
 	}
 
 	insert(key) {
+		this.heap.push(key)
+		this.swim(this.heap.length - 1)
+	}
 
+	delMax() {
+		let max = this.heap[0]
+		this.swap(0, this.heap.length - 1)
+		this.heap.pop()
+		this.sink(0)
+
+		return max
 	}
 
 	swim(k) {
         while(k > 0 && this.less(Math.floor(k/2), k)) {
-        	swap(Math.floor(k/2), k)
+        	this.swap(Math.floor(k/2), k)
         	k = Math.floor(k/2)
         }
 	}
 
 	sink(k) {
+		let j
+
         while(2 * k <= this.size()) {
         	j = 2 * k
         	if(this.less(j, j + 1)) { j++ }
         	if(!this.less(k, j)) { break }
-        	swap(k, j)
+        	this.swap(k, j)
             k = j
         }
 	}
@@ -41,4 +53,21 @@ class MaxHeap {
 	isEmpty() {
 		return this.size() === 0
 	}
+
+	toString() {
+		console.log(this.heap)
+	}
 }
+
+let heap = new MaxHeap()
+
+heap.insert(4)
+heap.insert(2)
+heap.insert(8)
+heap.insert(5)
+heap.insert(7)
+heap.insert(12)
+
+heap.toString()
+console.log(heap.delMax())
+heap.toString()
